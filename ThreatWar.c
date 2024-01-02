b83176ee67f33c6856e99f30808bfbf07c5fa872
cortex <webcnremi@gmail.com>
2024-01-02 20:39:16 +0100

    load background world

--- ThreatWar.c
+++ ThreatWar.c
@@ -1,10 +1,14 @@
 #include <SDL2/SDL.h>
+#include <stdlib.h>
+
+SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer);
 
 int main(int argc, char *argv[]) {
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
-	SDL_Color blue = {0, 95, 255, 1};
-
+	SDL_Color blue = {0, 0, 0, 1};
+	SDL_Texture *image = NULL;
+		
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
 		goto Quit;
 	
@@ -23,8 +27,15 @@ int main(int argc, char *argv[]) {
 
 	if (SDL_RenderClear(renderer) != 0) 
 		goto Quit;
-	
-	SDL_Delay(1);
+
+	image = loadImage("world.bmp", renderer);
+
+	if (image == NULL)
+		goto Quit;
+
+	SDL_Rect dstRect = {0, 0, 1920, 1080}; 
+    SDL_RenderCopy(renderer, image, NULL, &dstRect);
+
 	SDL_RenderPresent(renderer);
 	SDL_Delay(3000);
 	
@@ -33,4 +44,25 @@ int main(int argc, char *argv[]) {
 
 Quit:
 	SDL_Quit();
+}
+
+SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer) {
+	SDL_Surface *tmp = NULL;
+	SDL_Texture *texture = NULL;
+	tmp = SDL_LoadBMP(path);
+	
+	if (tmp == NULL) {
+		fprintf(stderr, "Error: %s", SDL_GetError());
+		return NULL;
+	}
+
+	texture = SDL_CreateTextureFromSurface(renderer, tmp);
+	SDL_FreeSurface(tmp);
+	
+	if (texture == NULL) {
+		fprintf(stderr, "Error: %s", SDL_GetError());
+		return NULL;
+	}
+	
+	return texture;
 }
